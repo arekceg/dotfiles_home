3f1fc5d3b957475c4266c7cb68793971b918c17d
arekceg <arekceg@protonmail.com>
2020-01-16 20:38:05 +0100

    Remove whole .config file from repo. Add only required items

--- .config/i3/i3lock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 25% -scale 400% -fill black -colorize 25% /tmp/screen.png
-convert /tmp/screen.png /home/arek/Pictures/lock.png -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
--- .config/wpg/wp_init.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-wpg -rsL 'josh-spires-dronenr-vm6CjQo2vOU-unsplash.jpg' 'josh-spires-dronenr-vm6CjQo2vOU-unsplash.jpg'
