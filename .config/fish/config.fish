9dc87e504094ca07d25653167daa14ebd3d03904
aceglowski <arekceg@protonmail.com>
2020-07-12 12:20:09 +0200

    * I forgot to commit for a long long time

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,3 @@
 set fish_key_bindings fish_user_key_bindings
 alias vim "gvim -v"
-alias ws "cd /home/aceglowski/Workspaces"
-alias trunk "cd /home/aceglowski/Workspaces/Trunk"
-alias k12 "cd /home/aceglowski/Workspaces/K12"
-alias k11 "cd /home/aceglowski/Workspaces/K11"
+xset r rate 300 50
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,1 +1,1 @@
-/home/aceglowski/.local/share/omf/themes/slavic-cat/fish_prompt.fish
+/home/arekceg/.local/share/omf/themes/slavic-cat/fish_prompt.fish
