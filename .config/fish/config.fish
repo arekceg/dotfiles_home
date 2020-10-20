50f2b378ab66aa3eb58c0dea530aa27c6fcabb77
aceglowski <arekceg@protonmail.com>
2020-10-21 00:18:52 +0200

    Polybar, autostart X
    * Add system tray to polybar config
    * Add X autostrart to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 set fish_key_bindings fish_user_key_bindings
 alias vim "gvim -v"
 xset r rate 300 50
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
